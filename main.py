bc0453af6b6ba7a840dd0912f035709c980eb1e8
imalka <itai257@gmail.com>
2021-07-01 01:28:47 +0300

    printing

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ for epoch in range(epochs):
         # print("tag_scores shape-", tag_scores.shape)
         # print("indices shape-", indices.shape)
         print("-------------------")
-        print("tagged_tree: {}, real_tree: {}".foramt(tagged_tree, true_tree_heads))
+        print("tagged_tree: {}, real_tree: {}".format(tagged_tree, true_tree_heads))
         acc = sum(tagged_tree == true_tree_heads[0]) / len(tagged_tree)
         acc_list.append(acc.item())
         #acc += torch.mean(torch.tensor(pos_idx_tensor.to("cpu") == indices.to("cpu"), dtype=torch.float))
