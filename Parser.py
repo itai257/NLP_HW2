abf307e13cb471ad8048c0ae659e04a30f346f7d
imalka <itai257@gmail.com>
2021-07-06 01:02:53 +0300

    added advanced model: word embeddings = glove word embeddings dimention = from 150 to 300 mlp function changed to: linear -> Relu -> Linear -> Relu -> Linear

--- Parser.py
+++ Parser.py
@@ -60,3 +60,72 @@ class KDependencyParserBasic(nn.Module):
         Z = torch.cat([X2, Y2], -1)
         Z = Z.view(-1, Z.shape[-1]).to(self.device)
         return Z
+
+
+
+
+
+# ----------------------------------------------------------------------------------
+
+
+
+class KDependencyParserAdvanced(nn.Module):
+    def __init__(self, word_vocab_size, tag_vocab_size, word_embed_dim, pos_embed_dim, biLSTM_hidden_dim,
+                 mlp_dim, pretrained_words_embeddings=None):
+        super(KDependencyParserAdvanced, self).__init__()
+
+        # init hyper parameters
+        self.word_embedding_dim = word_embed_dim
+        self.tag_embedding_dim = pos_embed_dim
+        self.biLSTM_hidden_dim = biLSTM_hidden_dim
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.biLSTM_in_size = self.word_embedding_dim + self.tag_embedding_dim
+        self.biLSTM_hidden_dim = self.biLSTM_in_size
+        self.hidden_dim_MLP = mlp_dim
+
+        if pretrained_words_embeddings is not None:
+            self.word_embedding = nn.Embedding.from_pretrained(pretrained_words_embeddings, freeze=True)
+        else:
+            self.word_embedding = nn.Embedding(word_vocab_size, self.word_embedding_dim)
+        self.pos_embedding = nn.Embedding(tag_vocab_size, self.tag_embedding_dim)
+
+        self.encoder = nn.LSTM(input_size=self.biLSTM_in_size, hidden_size=self.biLSTM_hidden_dim, num_layers=2,
+                               bidirectional=True)
+        self.decoder = decode_mst  # This is used to produce the maximum spannning tree during inference
+
+        self.mlp = nn.Sequential(
+            nn.Linear(self.biLSTM_hidden_dim * 2 * 2, self.hidden_dim_MLP),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim_MLP, self.hidden_dim_MLP),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim_MLP, 1))
+
+    def forward(self, sentence):
+        word_idx_tensor, pos_idx_tensor, true_tree_heads = sentence
+
+        # Pass word_idx and pos_idx through their embedding layers
+        word_embeds = self.word_embedding(word_idx_tensor.to(self.device))
+        pos_embeds = self.pos_embedding(pos_idx_tensor.to(self.device))
+
+        # Concat both embedding outputs
+        embeds = torch.cat((word_embeds, pos_embeds), 2) #[sentence_length, word_embed + pos_embed]
+        # Get Bi-LSTM hidden representation for each word+pos in sentence
+        lstm_out, _ = self.encoder(embeds.view(embeds.shape[1], 1, -1))  # -> [num of words in sentence, 1, hidden_dim*2]
+
+        # Get score for each possible edge in the parsing graph, construct score matrix
+        v_head_modifier_matrix = self.get_all_appended_head_mod(lstm_out)
+        score_matrix = self.mlp(v_head_modifier_matrix)
+
+        score_matrix_items = score_matrix.view(lstm_out.shape[0], lstm_out.shape[0])
+
+        return score_matrix_items.unsqueeze(0)
+
+    def get_all_appended_head_mod(self, lstm_out):
+        X = lstm_out.permute(1, 0, 2).squeeze(0)
+        X1 = X.unsqueeze(1)
+        Y1 = X.unsqueeze(0)
+        X2 = X1.repeat(1, X.shape[0], 1)
+        Y2 = Y1.repeat(X.shape[0], 1, 1)
+        Z = torch.cat([X2, Y2], -1)
+        Z = Z.view(-1, Z.shape[-1]).to(self.device)
+        return Z
---
+++ main_advanced.py
@@ -0,0 +1,157 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data.dataloader import DataLoader
+from DataLoad import get_vocabs, PosDataReader
+from DataLoad import PosDataset
+from Parser import KDependencyParserAdvanced
+import time
+from chu_liu_edmonds import decode_mst
+import matplotlib.pyplot as plt
+
+def loss_function(scores, real):
+    nll_loss = nn.NLLLoss(ignore_index=-1)
+    log_soft_max = nn.LogSoftmax(dim=1)
+    cur_loss = nll_loss(log_soft_max(scores), real)
+    return cur_loss
+
+def evaluate(model, data_loader):
+    with torch.no_grad():
+        accuracy = 0
+        words_count = 0
+        accumulated_loss = 0
+        for i, sentence in enumerate(data_loader):
+            words_idx_tensor, pos_idx_tensor, _, true_tree_heads = sentence
+
+            score_matrix = model((words_idx_tensor, pos_idx_tensor, true_tree_heads))
+            accumulated_loss += loss_function(score_matrix, true_tree_heads.to(device)).item()
+
+            score_matrix_numpy = score_matrix.squeeze(0).cpu().detach().numpy()
+            predicted_tree, _ = decode_mst(score_matrix_numpy, len(score_matrix_numpy), has_labels=False)
+            true_tree_heads = true_tree_heads.squeeze(0).cpu().detach().numpy()
+            accuracy += np.sum(true_tree_heads[1:] == predicted_tree[1:]) # compare without first word
+            words_count += len(predicted_tree)
+
+        accumulated_loss = accumulated_loss / len(data_loader)
+        accuracy = accuracy / words_count
+
+    return accuracy, accumulated_loss
+
+data_dir = "data/"
+path_train = data_dir + "train.labeled"
+print("path_train -", path_train)
+path_test = data_dir + "test.labeled"
+print("path_test -", path_test)
+paths_list = [path_train]
+
+
+word_dict, pos_dict = get_vocabs(paths_list)
+train = PosDataset(word_dict, pos_dict, data_dir, 'train', padding=False)
+train_dataloader = DataLoader(train, shuffle=True)
+test = PosDataset(word_dict, pos_dict, data_dir, 'test', padding=False)
+test_dataloader = DataLoader(test, shuffle=False)
+
+
+EPOCHS = 15
+WORD_EMBEDDING_DIM = 300
+POS_EMBEDDING_DIM = 25
+LSTM_HIDDEN_DIM = 125
+MLP_HIDDEN_DIM = 100
+LEARNING_RATE = 0.001
+ACCUMULATED_GRAD_STEPS = 50  # This is the actual batch_size, while we officially use batch_size=1
+
+word_vocab_size = len(train.word_idx_mappings)
+tag_vocab_size = len(train.pos_idx_mappings)
+
+model = KDependencyParserAdvanced(word_vocab_size, tag_vocab_size, WORD_EMBEDDING_DIM, POS_EMBEDDING_DIM,
+                               LSTM_HIDDEN_DIM, MLP_HIDDEN_DIM, train.word_vectors)
+
+use_cuda = torch.cuda.is_available()
+if use_cuda:
+    model.cuda()
+
+device = torch.device("cuda:0" if use_cuda else "cpu")
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+
+# Training start
+print("Training Started")
+model.train()
+train_accuracy_list = []
+test_accuracy_list = []
+
+train_loss_list = []
+test_loss_list = []
+
+loss_list = []
+epochs = EPOCHS
+
+for epoch in range(epochs):
+    epoch_start_time = time.time()
+    acc_list = []  # to keep track of accuracy
+    printable_loss = 0  # To keep track of the loss value
+    i = 0
+    for batch_idx, input_data in enumerate(train_dataloader):
+        i += 1
+        words_idx_tensor, pos_idx_tensor, sentence_length, true_tree_heads = input_data
+
+        score_matrix = model((words_idx_tensor, pos_idx_tensor, true_tree_heads))
+
+        loss = loss_function(score_matrix, true_tree_heads.to(device))
+        printable_loss += loss.item()
+        loss.backward()
+
+        if i % ACCUMULATED_GRAD_STEPS == 0 and i > 0:
+            optimizer.step()
+            model.zero_grad()
+    printable_loss = printable_loss / len(train)
+    loss_list.append(float(printable_loss))
+    print("evaluating epoch:")
+    test_accuracy, test_loss = evaluate(model, test_dataloader)
+    train_accuracy, train_loss = evaluate(model, train_dataloader)
+    test_loss_list.append(test_loss)
+    test_accuracy_list.append(test_accuracy)
+
+    train_loss_list.append(train_loss)
+    train_accuracy_list.append(train_accuracy)
+
+    e_interval = i
+
+    time_of_epoch = time.time() - epoch_start_time
+    epoch_print = "---\nEpoch {} Completed,\tTrain Loss {}\tTrain Accuracy: {}\t Test Loss {}\tTest Accuracy: {}\t \
+    time: {}".format(epoch + 1, train_loss, train_accuracy, test_loss, test_accuracy, time_of_epoch)
+    print(epoch_print)
+
+
+# show graphs:
+
+# train:
+fig, axs = plt.subplots(2, 1, constrained_layout=True)
+fig.suptitle('Train results - Advanced model', fontsize=16)
+axs[0].plot(np.arange(len(train_accuracy_list)), train_accuracy_list, c="red", label ="Accuracy")
+axs[0].set_title('Train accuracy')
+axs[0].set_xlabel('Epochs')
+axs[0].set_ylabel('Value')
+
+axs[1].plot(np.arange(len(train_loss_list)), train_loss_list, c="blue", label ="Loss")
+axs[1].set_xlabel('Epochs')
+axs[1].set_ylabel('Value')
+axs[1].set_title('Train loss')
+
+plt.show()
+
+# test:
+fig, axs = plt.subplots(2, 1, constrained_layout=True)
+fig.suptitle('Test results - Advanced model', fontsize=16)
+axs[0].plot(np.arange(len(test_accuracy_list)), test_accuracy_list, c="red", label ="Accuracy")
+axs[0].set_title('Test accuracy')
+axs[0].set_xlabel('Epochs')
+axs[0].set_ylabel('Value')
+
+axs[1].plot(np.arange(len(test_loss_list)), test_loss_list, c="blue", label ="Loss")
+axs[1].set_xlabel('Epochs')
+axs[1].set_ylabel('Value')
+axs[1].set_title('Test loss')
+
+plt.show()
